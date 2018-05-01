909e3ccbcf2879e9739fb3dd633962d904834304
Michael Potter <mgp140@gmail.com>
2018-05-01 16:05:44 -0400

    Added copy paste edit operations

--- main.js
+++ main.js
@@ -35,7 +35,19 @@ app.on("window-all-closed", function () {
 const mainMenuTemplate = [
   {
     label: 'File'
-  }
+  },
+  {
+    label: "Edit",
+    submenu: [
+      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+      { type: "separator" },
+      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]
+  },
 ]
 
 // Add Devtools
@@ -54,9 +66,7 @@ if (process.env.NODE_ENV !== 'production') {
         role: 'reload'
       }
     ],
-    label: 'New Window',
-  },
-  );
+  });
 }
 
 /**
