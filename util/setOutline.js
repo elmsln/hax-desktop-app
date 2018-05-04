492c8e56c72d6691bb6727822e046d508cae7c54
Michael Potter <mgp140@gmail.com>
2018-05-04 13:18:51 -0400

    pretty print outline.  remove state cruft.

--- util/setOutline.js
+++ util/setOutline.js
@@ -3,6 +3,16 @@ const _ = require('underscore');
 const fs = require('fs');
 
 module.exports = (location, outline) => {
+  // format the outline correctly
+  // if it's a string then convert it into JSON so it can be parsed
+  // correctly
+  outline = (typeof outline === 'string') ? JSON.parse(outline) : outline
+  // remove any cruft like children
+  outline = outline.map(i => {
+    delete(i.children)
+    return i
+  });
+  outline = JSON.stringify(outline, null, 2)
 
   const promise = new Promise((resolve, reject) => {
     try {
